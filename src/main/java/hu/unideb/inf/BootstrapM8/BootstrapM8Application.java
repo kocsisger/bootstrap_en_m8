468e36d18c2d2bb7155757e1e1f04525b9c318d4
John Smith <you@inf.unideb.hu>
2024-11-18 08:17:28 +0100

    Initial commit

---
+++ src/main/java/hu/unideb/inf/BootstrapM8/BootstrapM8Application.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.BootstrapM8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapM8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapM8Application.class, args);
+	}
+
+}
