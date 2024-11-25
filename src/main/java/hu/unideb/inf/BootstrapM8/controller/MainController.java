eba6590b1780ad9c4df123df85321718e7caf49f
John Smith <you@inf.unideb.hu>
2024-11-25 09:12:11 +0100

    Adding is working

--- src/main/java/hu/unideb/inf/BootstrapM8/controller/MainController.java
+++ src/main/java/hu/unideb/inf/BootstrapM8/controller/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -31,5 +32,11 @@ public class MainController {
     public String newPerson(Model model){
         model.addAttribute("newPerson", new Person());
         return "newPersonForm";
+    }
+
+    @PostMapping("/persons/save")
+    public String savePerson(Person newPerson){
+        personRepository.save(newPerson);
+        return "redirect:/persons";
     }
 }
